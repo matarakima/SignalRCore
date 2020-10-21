af2b3bf4b316355402a43d95a611f5da0988f71e
Andres Sanchez Muñoz <matarakima@gmail.com>
2020-10-21 11:19:19 -0500

    Initial content to signal r core

---
+++ SingalRCore.Mvc.Demo/SingalRCore.Mvc.Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SingalRCore.Mvc.Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
